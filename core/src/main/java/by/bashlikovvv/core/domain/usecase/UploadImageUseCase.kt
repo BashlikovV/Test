b55ea76440f9ceb410f58bc581621d0466636a16
bashlikovvv <volodia413314@gmail.com>
2024-03-24 14:44:50 +0300

    State processing during image uploading added.

--- core/src/main/java/by/bashlikovvv/core/domain/usecase/UploadImageUseCase.kt
+++ core/src/main/java/by/bashlikovvv/core/domain/usecase/UploadImageUseCase.kt
@@ -1,24 +1,33 @@
 package by.bashlikovvv.core.domain.usecase
 
+import android.net.ConnectivityManager
 import android.net.Uri
+import by.bashlikovvv.core.ext.isConnected
 import com.google.android.gms.tasks.Task
 import com.google.firebase.storage.FirebaseStorage
 
-class UploadImageUseCase(private val firebaseStorage: FirebaseStorage) {
+class UploadImageUseCase(
+    private val firebaseStorage: FirebaseStorage,
+    private val cm: ConnectivityManager?
+) {
 
-    fun execute(imageUri: Uri): Task<Uri>  {
-        val filename = imageUri.lastPathSegment.toString()
-        val ref = firebaseStorage.reference.child("images/$filename.png")
+    fun execute(imageUri: Uri): Task<Uri>?  {
+        return if (cm.isConnected()) {
+            val filename = imageUri.lastPathSegment.toString()
+            val ref = firebaseStorage.reference.child("images/$filename.png")
 
-        return ref.putFile(imageUri)
-            .continueWithTask { task ->
-                if (!task.isSuccessful) {
-                    task.exception?.let {
-                        throw it
+            ref.putFile(imageUri)
+                .continueWithTask { task ->
+                    if (!task.isSuccessful) {
+                        task.exception?.let {
+                            throw it
+                        }
                     }
+                    ref.downloadUrl
                 }
-                ref.downloadUrl
-            }
+        } else {
+            null
+        }
     }
 
 }
